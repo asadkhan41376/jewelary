889b72abdbe0eda3bf2e555605e741988f8ec0bf
mohammad asad <asadkhan41376@gmail.com>
2026-02-24 12:19:59 +0530

    some images logo chnages etc

--- assets/js/custom-gsap.js
+++ assets/js/custom-gsap.js
@@ -54,114 +54,114 @@ if ($(window).width() > 576 && $(".tw-char-animation").length > 0) {
 }
 
     // 9. Fade-Animation Js
-    // const fadeArrayup = gsap.utils.toArray(".tw_fade_anim");
-    // fadeArrayup.forEach((t, e) => {
-    //     var r = "bottom",
-    //         a = 1,
-    //         o = 1,
-    //         i = 50,
-    //         s = .5,
-    //         l = "power2.out";
-    //     t.getAttribute("data-fade-offset") && (i = t.getAttribute("data-fade-offset")), t.getAttribute("data-duration") && (o = t.getAttribute("data-duration")), t.getAttribute("data-fade-from") && (r = t.getAttribute("data-fade-from")), t.getAttribute("data-on-scroll") && (a = t.getAttribute("data-on-scroll")), t.getAttribute("data-delay") && (s = t.getAttribute("data-delay")), t.getAttribute("data-ease") && (l = t.getAttribute("data-ease")), 1 == a ? ("top" == r && gsap.from(t, {
-    //         y: -i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s,
-    //         scrollTrigger: {
-    //             trigger: t,
-    //             start: "top 110%"
-    //         }
-    //     }), "left" == r && gsap.from(t, {
-    //         x: -i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s,
-    //         scrollTrigger: {
-    //             trigger: t,
-    //             start: "top 110%"
-    //         }
-    //     }), "bottom" == r && gsap.from(t, {
-    //         y: i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s,
-    //         scrollTrigger: {
-    //             trigger: t,
-    //             start: "top 110%"
-    //         }
-    //     }), "right" == r && gsap.from(t, {
-    //         x: i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s,
-    //         scrollTrigger: {
-    //             trigger: t,
-    //             start: "top 110%"
-    //         }
-    //     }), "in" == r && gsap.from(t, {
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s,
-    //         scrollTrigger: {
-    //             trigger: t,
-    //             start: "top 110%"
-    //         }
-    //     })) : ("top" == r && gsap.from(t, {
-    //         y: -i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s
-    //     }), "left" == r && gsap.from(t, {
-    //         x: -i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s
-    //     }), "bottom" == r && gsap.from(t, {
-    //         y: i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s
-    //     }), "right" == r && gsap.from(t, {
-    //         x: i,
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s
-    //     }), "in" == r && gsap.from(t, {
-    //         opacity: 0,
-    //         ease: l,
-    //         duration: o,
-    //         delay: s
-    //     }))
-    // });
+    const fadeArrayup = gsap.utils.toArray(".tw_fade_anim");
+    fadeArrayup.forEach((t, e) => {
+        var r = "bottom",
+            a = 1,
+            o = 1,
+            i = 50,
+            s = .5,
+            l = "power2.out";
+        t.getAttribute("data-fade-offset") && (i = t.getAttribute("data-fade-offset")), t.getAttribute("data-duration") && (o = t.getAttribute("data-duration")), t.getAttribute("data-fade-from") && (r = t.getAttribute("data-fade-from")), t.getAttribute("data-on-scroll") && (a = t.getAttribute("data-on-scroll")), t.getAttribute("data-delay") && (s = t.getAttribute("data-delay")), t.getAttribute("data-ease") && (l = t.getAttribute("data-ease")), 1 == a ? ("top" == r && gsap.from(t, {
+            y: -i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s,
+            scrollTrigger: {
+                trigger: t,
+                start: "top 110%"
+            }
+        }), "left" == r && gsap.from(t, {
+            x: -i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s,
+            scrollTrigger: {
+                trigger: t,
+                start: "top 110%"
+            }
+        }), "bottom" == r && gsap.from(t, {
+            y: i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s,
+            scrollTrigger: {
+                trigger: t,
+                start: "top 110%"
+            }
+        }), "right" == r && gsap.from(t, {
+            x: i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s,
+            scrollTrigger: {
+                trigger: t,
+                start: "top 110%"
+            }
+        }), "in" == r && gsap.from(t, {
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s,
+            scrollTrigger: {
+                trigger: t,
+                start: "top 110%"
+            }
+        })) : ("top" == r && gsap.from(t, {
+            y: -i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s
+        }), "left" == r && gsap.from(t, {
+            x: -i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s
+        }), "bottom" == r && gsap.from(t, {
+            y: i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s
+        }), "right" == r && gsap.from(t, {
+            x: i,
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s
+        }), "in" == r && gsap.from(t, {
+            opacity: 0,
+            ease: l,
+            duration: o,
+            delay: s
+        }))
+    });
 
 
-//     document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener("DOMContentLoaded", function () {
 
-//   if (window.innerWidth <= 768) return;
+  if (window.innerWidth <= 768) return;
 
-//   gsap.from(".experience-wrapper", {
-//     y: 40,
-//     opacity: 0,
-//     duration: 0.6,
-//     stagger: 0.12,
-//     ease: "power2.out",
-//     scrollTrigger: {
-//       trigger: ".experience-area",
-//       start: "top 80%",
-//       toggleActions: "play none none none",
-//     }
-//   });
+  gsap.from(".experience-wrapper", {
+    y: 40,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.12,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: ".experience-area",
+      start: "top 80%",
+      toggleActions: "play none none none",
+    }
+  });
 
-// });
+});
 
 
 
